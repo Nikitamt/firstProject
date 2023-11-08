e1640a3a9409b44aacc8cb9c36701e07c3df112c
Nikitamt <106228571+Nikitamt@users.noreply.github.com>
2023-11-08 11:12:38 +0300

    add script for modal

--- yoga/js/scriptAge.js
+++ yoga/js/scriptAge.js
@@ -6,3 +6,25 @@ alert("Пользователь " + surname + " " + name + ", его возра�
 showUser();  */}
 
 // showUser(age, 'Горький', 'Максим');
+
+class Options {
+  
+  constructor(height, width, bg, fontSize, textAlign) {
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.textAlign = textAlign;
+  }
+
+  createDiv() {
+    let div = document.createElement('div');
+    div.textContent = 'Hello World';
+    document.body.appendChild(div);
+    const param = `height: ${this.height}px; width: ${this.width}px; background-color: ${this.bg}; font-size: ${this.fontSize}px; text-align: ${this.textAlign};`;
+    div.style.cssText = param;
+  }
+}
+
+const item = new Options(300, 350, 'red', 18, 'center');
+item.createDiv();
